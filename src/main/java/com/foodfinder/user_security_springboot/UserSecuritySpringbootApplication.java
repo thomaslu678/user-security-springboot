bfd3fb199bd35a10a048bf7ba300b915d8ac1d71
thomaslu678 <thomaslu678@gmail.com>
2025-01-05 11:22:18 -0700

    first commit

---
+++ src/main/java/com/foodfinder/user_security_springboot/UserSecuritySpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.foodfinder.user_security_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSecuritySpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSecuritySpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodfinder/user_security_springboot/UserSecuritySpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodfinder.user_security_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSecuritySpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
